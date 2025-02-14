d09e22726a8b218ac593f19c8155bfdae7ee87d2
LAS90 <113226094+LAS90@users.noreply.github.com>
2025-02-14 17:23:08 +0700

    add Dockerfiles

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -5,19 +5,28 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.WebHost.UseUrls("http://0.0.0.0:5000");
+
 builder.Services.AddGrpcClient<AuthServiceClient>(o =>
 {
-    o.Address = new Uri("https://localhost:5001"); // URL AuthService
+    o.Address = new Uri(builder.Configuration["AuthService:GrpcUrl"]);
+    o.ChannelOptionsActions.Add(opt => opt.HttpHandler = new SocketsHttpHandler
+    {
+        PooledConnectionIdleTimeout = Timeout.InfiniteTimeSpan,
+        KeepAlivePingDelay = TimeSpan.FromSeconds(15),
+        KeepAlivePingTimeout = TimeSpan.FromSeconds(5),
+        EnableMultipleHttp2Connections = true
+    });
+    o.ChannelOptionsActions.Add(opt => opt.Credentials = Grpc.Core.ChannelCredentials.Insecure);
 });
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
-
+//}
 
 app.UseAuthorization();
 app.MapControllers();
